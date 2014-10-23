17b2e2ce89470eb8b1d9e25a24035170b3dd0f05
Damian Kaczmarek <rush@virtkick.io>
2014-10-23 22:07:26 +0200

    Capture also .json

--- master.js
+++ master.js
@@ -15,40 +15,45 @@ var masterConfig = {
   railsHost: process.env.RAILS_HOST || 'localhost'
 };
 
+var routeVnc = [
+  function(config, commService) {
+    var http = require('http');
+
+    return {
+      requestHandler: function(req, res, next) {
+        var targetMachine = parseInt(req.match[0]);
+        if(!targetMachine) return next();
+
+        http.get({
+          hostname: config.railsHost,
+          port: config.railsPort,
+          headers: req.headers,
+          path: '/machines/' + targetMachine + '/vnc.json'
+        }, function(res) {
+          res.setEncoding('utf8');
+          res.on('data', function(data) {
+            try {
+              data = JSON.parse(data);
+              if(data.port == -1)
+                next();
+              req.match = data;
+              next();
+            } catch(err) {
+              console.log(err);
+              next();
+            }
+          });
+        });
+      }
+    };
+  }, 'websockify -> [host]:[port]' ];
+
+
 var portImplementation = {
   router: {
     // BELOW FUNCTION SEES ONLY ITS CONTEXT, IT IS SERIALIZED TO THE WORKERS
-    '/machines/*/vnc' : [function(config, commService) {
-      var http = require('http');
-
-      return {
-        requestHandler: function(req, res, next) {
-          var targetMachine = parseInt(req.match[0]);
-          if(!targetMachine) return next();
-
-          http.get({
-            hostname: config.railsHost,
-            port: config.railsPort,
-            headers: req.headers,
-            path: '/machines/' + targetMachine + '/vnc.json'
-          }, function(res) {
-            res.setEncoding('utf8');
-            res.on('data', function(data) {
-              try {
-                data = JSON.parse(data);
-                if(data.port == -1)
-                  next();
-                req.match = data;
-                next();
-              } catch(err) {
-                console.log(err);
-                next();
-              }
-            });
-          });
-        }
-      };
-    }, 'websockify -> [host]:[port]' ],
+    '/machines/*/vnc' : routeVnc,
+    '/machines/*/vnc.json' : routeVnc,
     '*' : masterConfig.railsPort
   }
 };
