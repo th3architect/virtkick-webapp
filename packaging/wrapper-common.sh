8eaf898a114fdda2b835ca53975980f687cc2db0
Damian Kaczmarek <rush@rushbase.net>
2014-12-15 12:24:13 +0100

    Polish starter scripts in the package

---
+++ packaging/wrapper-common.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+# Figure out where this script is located.
+SELFDIR="`dirname \"$0\"`"
+SELFDIR="`cd \"$SELFDIR\" && pwd`"
+
+# Tell Bundler where the Gemfile and gems are.
+export BUNDLE_GEMFILE="$SELFDIR/lib/vendor/Gemfile"
+unset BUNDLE_IGNORE_CONFIG
+
+cd "$SELFDIR/lib/app"
+
+export PATH="$SELFDIR/lib/ruby/bin:$SELFDIR/lib/vendor/ruby/2.1.0/bin:$PATH"
+if ! [ -e .devise-secret ];then
+    hexdump -n 16 -v -e '/1 "%02X"' /dev/urandom > .devise-secret
+fi
+if ! [ -e .cookie-secret ];then
+    hexdump -n 16 -v -e '/1 "%02X"' /dev/urandom > .cookie-secret
+fi
+
+export DEVISE_SECRET_KEY="$(cat .devise-secret)"
+export SECRET_KEY_BASE="$(cat .cookie-secret)"
+export PACKAGING=1 # prevent errors from missing development/test dependencies
+if [ "$RAILS_ENV" == "" ];then
+    export RAILS_ENV=production
+fi
+if ! [ -e db/$RAILS_ENV.sqlite3 ];then
+    "$SELFDIR/lib/ruby/bin/bundle" exec rake db:migrate
+fi
+
